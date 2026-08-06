aaa02ed4a046cf3cfbe20e8f922987de76815e9c
Ivan Bondarev <ibondarev17@gmail.com>
2026-08-06 13:37:18 +0100

    port editor options and keymaps from vimrc to lua

--- config_nvim/init.lua
+++ config_nvim/init.lua
@@ -1,1 +1,11 @@
+-- nvim-tree requires netrw to be disabled before anything else loads.
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- Must be set before lazy.nvim loads so plugin `keys` specs resolve correctly.
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+require("config.options")
+require("config.keymaps")
 require("config.lazy")
